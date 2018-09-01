e6734a5770f6a228856e9cc988360ee9e9a90560
The Mac Admin <miles@themacadmin.com>
2018-08-31 17:56:07 -0700

    Update Computer Information.bash

--- Computer Information.bash
+++ Computer Information.bash
@@ -3,6 +3,20 @@
 <<ABOUT_THIS_SCRIPT
 -----------------------------------------------------------------------
 
+Modified by:
+Miles Leacy
+Technical Expert: Apple Technology
+Walmart
+
+Modifed on: 2018 08 31
+
+Modification summary:
+* Simplified and genericized code
+* Adjusted data points and formatting based on individual use case
+* Simplified dialog and removed "Run Additional Commands"
+* Added Jamf inventory update at end
+
+========== Original Info Follows ==========
 	Written by:William Smith
 	Professional Services Engineer
 	Jamf
@@ -42,186 +56,234 @@ INSTRUCTIONS
 -----------------------------------------------------------------------
 ABOUT_THIS_SCRIPT
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# FUNCTIONS
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+simplifyBytes() {
+    b=${1:-0}; d=''; s=0; S=(Bytes {K,M,G,T,P,E,Z,Y}B)
+    while ((b > 1024)); do
+        d="$(printf ".%02d" $((b % 1024 * 100 / 1024)))"
+        b=$((b / 1024))
+        let s++
+    done
+    echo "$b$d ${S[$s]}"
+}
 
-## General section #####
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# SYSTEM VARIABLES
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+timeStamp=$(date +"%F %T")
 
-# Display computer name
-runCommand=$( /usr/sbin/scutil --get ComputerName )
-computerName="Computer Name: $runCommand"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Data Collection for Output
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-
-# Display serial number
-runCommand=$( /usr/sbin/system_profiler SPHardwareDataType | /usr/bin/grep "Serial Number" | /usr/bin/awk -F ": " '{ print $2 }' )
-serialNumber="Serial Number: $runCommand"
-
-
-# Display uptime
-runCommand=$( /usr/bin/uptime | /usr/bin/awk -F "(up |, [0-9] users)" '{ print $2 }' )
-if [[ "$runCommand" = *day* ]] || [[ "$runCommand" = *sec* ]] || [[ "$runCommand" = *min* ]] ; then
-	upTime="Uptime: $runCommand"
-else
-	upTime="Uptime: $runCommand hrs/min"
-fi
-
-
-
-## Network section #####
-
-
-# Display active network services and IP Addresses
-
-networkServices=$( /usr/sbin/networksetup -listallnetworkservices | /usr/bin/grep -v asterisk )
-
-while IFS= read aService
-do
-	activePort=$( /usr/sbin/networksetup -getinfo "$aService" | /usr/bin/grep "IP address" | /usr/bin/grep -v "IPv6" )
-	if [ "$activePort" != "" ] && [ "$activeServices" != "" ]; then
-		activeServices="$activeServices\n$aService $activePort"
-	elif [ "$activePort" != "" ] && [ "$activeServices" = "" ]; then
-		activeServices="$aService $activePort"
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# General
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	
+	# Get computer name
+	computerName="Computer Name: $( /usr/sbin/scutil --get ComputerName ) (for macOS)"
+	
+	# Get host name
+	hostName="Host Name: $( /usr/sbin/scutil --get HostName ) (for AD)"
+	
+	# Get operating system
+	operatingSystemVersion=$( /usr/bin/sw_vers -productVersion)
+	operatingSystemBuild=$( /usr/bin/sw_vers -buildVersion)
+	case $(echo $operatingSystemVersion | awk -F "." '{ print $2 }') in
+		14)
+			operatingSystemName="macOS Mojave"
+			;;
+		 13)
+			operatingSystemName="macOS High Sierra"
+			;;
+		12)
+			operatingSystemName="macOS Sierra"
+			;;
+		11)
+			operatingSystemName="OS X El Capitan"
+			;;
+		*)
+			operatingSystemName="Unsupported macOS Version"
+			;;
+		esac
+	osDisplay="OS: $operatingSystemName v$operatingSystemVersion ($operatingSystemBuild)"
+	
+	# Get serial number
+	serialNumber="Serial Number: $( ioreg -l | awk -F'"' '/IOPlatformSerialNumber/ { print $4;}' )"
+	
+	# Get Model Name
+	modelIdentifier=$(sysctl hw.model | awk '{print $2}')
+	marketingModel="$(defaults read /System/Library/PrivateFrameworks/ServerInformation.framework/Versions/A/Resources/English.lproj/SIMachineAttributes.plist "$modelIdentifier"|sed -n -e 's/\\//g' -e 's/.*marketingModel = "\(.*\)";/\1/p')"
+	modelDisplay="Model: $marketingModel"
+	
+	# Get uptime
+	runCommand=$( /usr/bin/uptime | /usr/bin/awk -F "(up |, [0-9] users)" '{ print $2 }' )
+	if [[ "$runCommand" = *day* ]] || [[ "$runCommand" = *sec* ]] || [[ "$runCommand" = *min* ]] ; then
+		upTime="Uptime: $runCommand"
+	else
+		upTime="Uptime: $runCommand hrs/min"
 	fi
-done <<< "$networkServices"
 
-activeServices=$( echo "$activeServices" | /usr/bin/sed '/^$/d')
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# Hardware
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	
+	# Get RAM
+	bytes=$(sysctl hw.memsize | awk '{print $2'})
+	totalRam="Total RAM: $(simplifyBytes $bytes)"
+	
+	# Get free space on /
+	FreeSpace=$( /usr/sbin/diskutil info / | /usr/bin/awk '/Free Space/ || /Available Space/ {print $4 " " $5}')
+	FreePercentage=$( /usr/sbin/diskutil info / | /usr/bin/awk -F'[\(\)]' '/Free Space/ || /Available Space/ {print $6}' )
+	diskSpace="Disk Space: $FreeSpace free ($FreePercentage available)"
+	
+	# Display battery cycle count
+	if [[ "$marketingModel" = *Book* ]]; then
+		batteryCycleCount="Battery Cycle Count: $( /usr/sbin/ioreg -r -c "AppleSmartBattery" | /usr/bin/awk '/\"CycleCount\"/ {print $3}' )"
+	fi
+	
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# Network
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+	
+	# Display active network services and IP Addresses
+	
+	networkServices=$( /usr/sbin/networksetup -listallnetworkservices | /usr/bin/grep -v asterisk )
+	
+	while IFS= read aService
+	do
+		activePort=$( /usr/sbin/networksetup -getinfo "$aService" | /usr/bin/grep "IP address" | /usr/bin/grep -v "IPv6" )
+		if [ "$activePort" != "" ] && [ "$activeServices" != "" ]; then
+			activeServices="$activeServices\n$aService $activePort"
+		elif [ "$activePort" != "" ] && [ "$activeServices" = "" ]; then
+			activeServices="$aService $activePort"
+		fi
+	done <<< "$networkServices"
+	
+	activeServices=$( echo "$activeServices" | /usr/bin/sed '/^$/d')
+	
+	
+	# Display Wi-Fi SSID
+	model=$( /usr/sbin/system_profiler SPHardwareDataType | /usr/bin/grep 'Model Name' )
+	
+	if [[ "$model" = *Book* ]]; then
+		runCommand=$( /usr/sbin/networksetup -getairportnetwork en0 | /usr/bin/awk -F ": " '{ print $2 }' )
+	else
+		runCommand=$( /usr/sbin/networksetup -getairportnetwork en1 | /usr/bin/awk -F ": " '{ print $2 }' )
+	fi
+	SSID="SSID: $runCommand"
+	
+	# Display SSH status
+	runCommand=$( /usr/sbin/systemsetup -getremotelogin | /usr/bin/awk -F ": " '{ print $2 }' ) 
+	SSH="SSH: $runCommand"
+	
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# Management
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+	
+	# Get JSS
+	jss=$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)
+	if [ $? -ne 0 ]; then
+	    managedBy="Managed by: There is a problem with this computer's management framework."
+	else
+	    managedBy="Managed by: $jss."
+	fi
+	
+	# Get JSS availability
+	jamf checkjssconnection -retry 1
+	jssAvailable="$?"
+	if [ $jssAvailable -eq 0 ]; then
+	    jssReachable="     Management server is reachable."
+	else
+	    jssReachable="     Management cannot be reached."
+	fi
+	
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# Active Directory
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+	
+	# Display Active Directory binding
+	boundFqDomain=$( /usr/sbin/dsconfigad -show | /usr/bin/grep "Directory Domain" | /usr/bin/awk -F "= " '{ print $2 }' )
+	boundDomain=$( echo $boundFqDomain | /usr/bin/awk -F'[.]' '{print toupper($1)}')
+	
+	if [ "$boundDomain" = "" ]; then
+		AD="Domain: None"
+	else
+		AD="Domain: $boundFqDomain"	
+	fi
+	
+	# Test Active Directory binding
+	bindTest=$( /usr/bin/dscl "/Active Directory/"$boundDomain"/All Domains" read /Users )
+	
+	if [ "$bindTest" = "name: dsRecTypeStandard:Users" ]; then
+		testAD="AD Connection Test: Successful"
+	else
+		testAD="AD Connection Test: Failed"	
+	fi
 
-
-# Display Wi-Fi SSID
-model=$( /usr/sbin/system_profiler SPHardwareDataType | /usr/bin/grep 'Model Name' )
-
-if [[ "$model" = *Book* ]]; then
-	runCommand=$( /usr/sbin/networksetup -getairportnetwork en0 | /usr/bin/awk -F ": " '{ print $2 }' )
-else
-	runCommand=$( /usr/sbin/networksetup -getairportnetwork en1 | /usr/bin/awk -F ": " '{ print $2 }' )
-fi
-
-SSID="SSID: $runCommand"
-
-
-# Display SSH status
-runCommand=$( /usr/sbin/systemsetup -getremotelogin | /usr/bin/awk -F ": " '{ print $2 }' ) 
-SSH="SSH: $runCommand"
-
-
-# Display date, time and time zone
-runCommand=$( /bin/date )
-timeInfo="Date and Time: $runCommand"
-
-
-# Display network time server
-runCommand=$( /usr/sbin/systemsetup -getnetworktimeserver )
-timeServer="$runCommand"
-
-
-
-## Active Directory section #####
-
-
-# Display Active Directory binding
-runCommand=$( /usr/sbin/dsconfigad -show | /usr/bin/grep "Directory Domain" | /usr/bin/awk -F "= " '{ print $2 }' )
-
-if [ "$runCommand" = talkingmoose.pvt ]; then
-	AD="Bound to Active Directory: Yes"
-else
-	AD="Bound to Active Directory: No"	
-fi
-
-
-# Test Active Directory binding
-runCommand=$( /usr/bin/dscl "/Active Directory/TALKINGMOOSE/All Domains" read /Users )
-
-if [ "$runCommand" = "name: dsRecTypeStandard:Users" ]; then
-	testAD="Test Active Directory Connection: Success"
-else
-	testAD="Test Active Directory Connection: Fail"	
-fi
-
-
-
-## Hardware/Software section #####
-
-
-# Display free space
-FreeSpace=$( /usr/sbin/diskutil info "Macintosh HD" | /usr/bin/grep  -E 'Free Space|Available Space' | /usr/bin/awk -F ":\s*" '{ print $2 }' | awk -F "(" '{ print $1 }' | xargs )
-FreePercentage=$( /usr/sbin/diskutil info "Macintosh HD" | /usr/bin/grep -E 'Free Space|Available Space' | /usr/bin/awk -F "(\\\(|\\\))" '{ print $6 }' )
-diskSpace="Disk Space: $FreeSpace free ($FreePercentage available)"
-
-
-# Display operating system
-runCommand=$( /usr/bin/sw_vers -productVersion)
-operatingSystem="Operating System: $runCommand"
-
-
-# Display battery cycle count
-runCommand=$( /usr/sbin/ioreg -r -c "AppleSmartBattery" | /usr/bin/grep -w "CycleCount" | /usr/bin/awk '{print $3}' | /usr/bin/sed s/\"//g )
-batteryCycleCount="Battery Cycle Count: $runCommand"
-
-
-
-## Format information #####
-
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Format Output
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 displayInfo="----------------------------------------------
 GENERAL
 
 $computerName
+$hostName
+$osDisplay
 $serialNumber
+$modelDisplay
 $upTime
+
+----------------------------------------------
+HARDWARE
+
+$totalRam
+$diskSpace
+$batteryCycleCount
+
 ----------------------------------------------
 NETWORK
 
 $activeServices
 $SSID
 $SSH
-$timeInfo
-$timeServer
+
+----------------------------------------------
+MANAGEMENT
+
+$managedBy
+$jssReachable
+
 ----------------------------------------------
 ACTIVE DIRECTORY
 
 $AD
 $testAD
-----------------------------------------------
-HARDWARE/SOFTWARE
 
-$diskSpace
-$operatingSystem
-$batteryCycleCount
 ----------------------------------------------"
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Display Output
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+# echo to stdout for testing
+echo "$displayInfo"
 
-## Display information to end user #####
+osascript -e "Tell application \"System Events\" to display dialog \"$displayInfo\" with title \"Computer Information\" with icon file posix file \"/System/Library/CoreServices/Finder.app/Contents/Resources/Finder.icns\" buttons {\"OK\"} default button {\"OK\"})"
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Update Jamf Inventory
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-runCommand="button returned of (display dialog \"$displayInfo\" with title \"Computer Information\" with icon file posix file \"/System/Library/CoreServices/Finder.app/Contents/Resources/Finder.icns\" buttons {\"Enable Remote Support\", \"OK\"} default button {\"OK\"})"
-
-clickedButton=$( /usr/bin/osascript -e "$runCommand" )
-
-
-
-## Run additional commands #####
-
-
-if [ "$clickedButton" = "Enable Remote Support" ]; then
-
-	# open a remote support application
-	# /usr/bin/open -a "/Applications/TeamViewer.app"
-
-	# run a policy to enable remote support
-	# /usr/local/bin/jamf policy -event EnableRemoteSupport
-	
-	# email computer information to help desk
-	currentUser=$( stat -f "%Su" /dev/console )
-	sudo -u "$currentUser" /usr/bin/open "mailto:support@talkingmoose.pvt?subject=Computer Information ($serialNumber)&body=$displayInfo"
-	
-	if [ $? = 0 ]; then
-		/usr/bin/osascript -e 'display dialog "Remote support enabled." with title "Computer Information" with icon file posix file "/System/Library/CoreServices/Finder.app/Contents/Resources/Finder.icns" buttons {"OK"} default button {"OK"}' &
-	else
-		/usr/bin/osascript -e 'display dialog "Failed enabling remote support." with title "Computer Information" with icon file posix file "/System/Library/CoreServices/Finder.app/Contents/Resources/Finder.icns" buttons {"OK"} default button {"OK"}' &
-	fi
-	
+if [ $jssAvailable -eq 0 ]; then
+	: # jamf recon
+else
+	printf "$timeStamp %s\n" "JSS is unavailable, unable to update inventory."
 fi
-
 
 exit 0
